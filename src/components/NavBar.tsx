6823d67afbba90f346641d946b4289a0a7eb576c
v <singhvirat1890@gmail.com>
2025-08-15 08:48:04 +0530

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -49,7 +49,7 @@ const NavBar = () => {
               <div className="w-8 h-8 bg-green-600 rounded-full flex items-center justify-center">
                 <span className="text-white font-bold text-sm">A</span>
               </div>
-              <span className="font-bold text-xl text-gray-900">AeroFarm Pro</span>
+              <span className="font-bold text-xl text-gray-900"> BloomSync </span>
             </button>
           </div>
 
